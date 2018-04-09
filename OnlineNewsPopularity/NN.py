0201ae66ef1512d5b270603a8ad63e5000e335e6
Aeshaan Wahlang <aeshaan.wahlang@gmail.com>
2018-04-09 19:05:44 -0400

    added input normalization

--- OnlineNewsPopularity/NN.py
+++ OnlineNewsPopularity/NN.py
@@ -3,10 +3,9 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
+from sklearn import preprocessing
 from PreProcessing import parse
 
-# fix random seed for reproducibility
-np.random.seed(7)
 data = pd.read_csv('OnlineNewsPopularity.csv')
 data = parse(data)
 
@@ -19,7 +18,10 @@ train_in = train[:, 0:57]
 train_out = train[:, 58]
 test_in = test[:, 0:57]
 test_out = test[:, 58]
-#one hot encoding for classes
+#one hot encoding for classes and normalization for inputs
+min_max = preprocessing.MinMaxScaler()
+train_in = min_max.fit_transform(train_in)
+test_in = min_max.fit_transform(test_in)
 train_out = to_categorical(train_out)
 test_out = to_categorical(test_out)
 #remove 0s columns 
@@ -28,13 +30,13 @@ test_out = np.delete(test_out, np.s_[0], axis = 1)
 
 #Network Model
 model = Sequential()
-model.add(Dense(200, input_dim = 57, activation = 'relu'))
-model.add(Dense(200, activation = 'relu'))
-model.add(Dense(200, activation = 'relu'))
+model.add(Dense(100, input_dim = 57, activation = 'relu'))
+model.add(Dense(100, activation = 'relu'))
+model.add(Dense(100, activation = 'relu'))
 model.add(Dense(3,activation = 'sigmoid'))
 
 model.compile(loss = 'binary_crossentropy', optimizer= 'adam', metrics=['accuracy'])
-model.fit(train_in, train_out, epochs = 10, batch_size= 5)
+model.fit(train_in, train_out, epochs = 10, batch_size= 20)
 
 #model evaluation
 scores = model.evaluate(test_in, test_out)
